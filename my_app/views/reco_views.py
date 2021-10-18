4b28f23965cd3ea5eb6673ee603665e1799c24c6
Jang-Boa <boajang97@gmail.com>
2021-10-19 00:45:30 +0900

    update recommend function

--- my_app/views/reco_views.py
+++ my_app/views/reco_views.py
@@ -1,8 +1,9 @@
 # recommend route
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, request, g
 
 from my_app import db
 from my_app.service.uploader import save_csv, insert_file
+from my_app.models.models import Car
 
 bp = Blueprint('reco', __name__, url_prefix='/recommend')
 
@@ -14,6 +15,10 @@ def recommend():
     return render_template('reco/prefer.html')
 
 # 추천 결과 페이지
-@bp.route('/results/')
+@bp.route('/results/',methods=('GET','POST'))
 def result():
-    return render_template('reco/results.html')
+    if request.method == 'GET':
+        manufacturer_text = request.args.get('manufacturer')
+        if manufacturer_text != 0:
+            select = Car.query.filter_by(company=manufacturer_text).first_or_404()
+        return render_template('reco/results.html',select=select)
